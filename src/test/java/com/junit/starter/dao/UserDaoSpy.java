6e307696acb81789fe8546b5030e9d6be1098723
Sergei-JD <sergshlyazhko@gmail.com>
2022-02-01 18:43:22 +0300

    added Mockito.Spy

--- src/test/java/com/junit/starter/dao/UserDaoSpy.java
+++ src/test/java/com/junit/starter/dao/UserDaoSpy.java
@@ -3,13 +3,19 @@ package com.junit.starter.dao;
 import java.util.HashMap;
 import java.util.Map;
 
-public class UserDaoMock extends UserDao {
+public class UserDaoSpy extends UserDao {
 
+    private final UserDao userDao;
     private Map<Integer, Boolean> answers = new HashMap<>();
+
+    public UserDaoSpy(UserDao userDao) {
+        this.userDao = userDao;
+    }
 //    private Answer1<Integer, Boolean> answer1;
 
     @Override
     public boolean delete(Integer userId) {
-        return answers.getOrDefault(userId, false);
+        // invocation++;
+        return answers.getOrDefault(userId, userDao.delete(userId));
     }
 }
