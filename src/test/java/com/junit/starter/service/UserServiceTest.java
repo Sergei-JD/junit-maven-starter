3c2550c5e7bb849889227b73318fed1fc4919786
Sergei-JD <sergshlyazhko@gmail.com>
2022-02-01 13:08:29 +0300

    added:     Launcher API;     libraries comparison: AssertJ & Hamcrest;     Testing exceptions;     Tagging and Filtering

--- src/test/java/com/junit/starter/service/UserServiceTest.java
+++ src/test/java/com/junit/starter/service/UserServiceTest.java
@@ -1,16 +1,132 @@
 package com.junit.starter.service;
 
-import org.junit.jupiter.api.Test;
+import com.junit.starter.dto.User;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.collection.IsMapContaining;
+import org.junit.jupiter.api.*;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import java.util.Map;
+import java.util.Optional;
 
-class UserServiceTest {
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.collection.IsEmptyCollection.empty;
+import static org.junit.jupiter.api.Assertions.*;
+
+@Tag("fast")
+//@Tag("login")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class UserServiceTest {
+
+    private static final User IVAN = User.of(1, "Ivan", "123");
+    private static final User PETR = User.of(2, "Petr", "111");
+
+    private UserService userService;
+
+    @BeforeAll
+    void init() {
+        System.out.println("Before all: " + this);
+    }
+
+    @BeforeEach
+    void prepare() {
+        System.out.println("Before each: " + this);
+        userService = new UserService();
+    }
 
     @Test
     void usersEmptyIfNoUserAdded() {
-        var userService = new UserService();
+        System.out.println("Test 1: " + this);
         var users = userService.getAll();
-        assertFalse(users.isEmpty(), () -> "User list should be empty");
+
+        // hamcrest
+        MatcherAssert.assertThat(users, empty());
+
+        // assertj
+        assertTrue(users.isEmpty(), () -> "User list should be empty");
         // input ->[box == func] -> actual output
+    }
+
+    @Test
+    void usersSizeIfUserAdded() {
+        System.out.println("Test 2: " + this);
+        userService.add(IVAN);
+        userService.add(PETR);
+
+        var users = userService.getAll();
+
+        assertThat(users).hasSize(2);
+//        assertEquals(2, users.size());
+    }
+
+    @Test
+    @Tag("login")
+    void loginSuccessIfUserExists() {
+        userService.add(IVAN);
+        Optional<User> maybeUser = userService.login(IVAN.getUsername(), IVAN.getPassword());
+        // assertj
+        assertThat(maybeUser).isPresent();
+        maybeUser.ifPresent(user -> assertThat(user).isEqualTo(IVAN));
+
+        // junit standart
+//        assertTrue(maybeUser.isPresent());
+//        maybeUser.ifPresent(user -> assertEquals(IVAN, user));
+    }
+
+    @Test
+    @Tag("login")
+    void throwExceptionIfUsernameOrPasswordIsNull() {
+        assertAll(
+                () -> {
+                    var exception = assertThrows(IllegalArgumentException.class, () -> userService.login(null, "dummy"));
+                    assertThat(exception.getMessage()).isEqualTo("username or password is null");
+                },
+                () -> assertThrows(IllegalArgumentException.class, () -> userService.login("dummy", null))
+        );
+    }
+
+    @Test
+    void usersConvertedToMapById() {
+        userService.add(IVAN, PETR);
+
+        Map<Integer, User> users = userService.getAllConvertedById();
+
+        // hamcrest
+        MatcherAssert.assertThat(users, IsMapContaining.hasKey(IVAN.getId()));
+
+        // assertj
+        assertAll(
+                () -> assertThat(users).containsKeys(IVAN.getId(), PETR.getId()),
+                () -> assertThat(users).containsValues(IVAN, PETR)
+        );
+    }
+
+    @Test
+    @Tag("login")
+    void loginFailIfPasswordIsNotCorrect() {
+        userService.add(IVAN);
+
+        var maybeUser = userService.login(IVAN.getUsername(), "dummy");
+
+        assertTrue(maybeUser.isEmpty());
+    }
+
+    @Test
+    @Tag("login")
+    void loginFailIfUserDoesNotExist() {
+        userService.add(IVAN);
+
+        var maybeUser = userService.login("dummy", IVAN.getPassword());
+
+        assertTrue(maybeUser.isEmpty());
+    }
+
+    @AfterEach
+    void deleteDataFromDatabase() {
+        System.out.println("After each: " + this);
+    }
+
+    @AfterAll
+    void closeConnectionPool() {
+        System.out.println("After all: " + this);
     }
 }
